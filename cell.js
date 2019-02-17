9482013776057ec5f50198fc42dc736cfea2a421
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-17 10:31:44 +0100

    Added click function and optimized parameters

--- cell.js
+++ cell.js
@@ -1,10 +1,21 @@
 class Cell {
-    constructor(x,y) {
+    constructor(x,y,size) {
        this.x = x; 
        this.y = y; 
+       this.size = size;
+       this.col = "#123654";
     }
 
     show = () => {
-        rect(this.x,this.y,50,50);
+        fill(this.col)
+        rect(this.x,this.y,this.size,this.size);
+    }
+
+    clicked = () => {
+        let d = dist(mouseX, mouseY, this.x+this.size/2, this.y+this.size/2);
+        if(d<=this.size/2) {
+            this.col = "#fff";
+            console.log("Click")
+        }
     }
 }
