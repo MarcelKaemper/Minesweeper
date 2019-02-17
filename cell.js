793c043f48dadda2e91896fcd4e3b760b6ef3302
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-17 11:22:34 +0100

    Checks if the player lost the game

--- cell.js
+++ cell.js
@@ -4,20 +4,25 @@ class Cell {
        this.y = y; 
        this.size = size;
        this.col = color;
+       this.revealed = false;
        this.text = text;
     }
 
     show = () => {
-        fill(this.col)
+        this.revealed?fill(this.col):fill(200)
         rect(this.x,this.y,this.size,this.size);
         fill(128);
-        text(this.text,this.x+25,this.y+25);
+        this.revealed?text(this.text,this.x+25,this.y+25):text("",0,0);
     }
 
     clicked = () => {
         let d = dist(mouseX, mouseY, this.x+this.size/2, this.y+this.size/2);
         if(d<=this.size/2) {
-            this.col = "#fff";
+            if(this.text == "x"){
+                alert("You lost!");
+                location.reload();
+            }
+            this.revealed = true;
             console.log("Click")
         }
     }
