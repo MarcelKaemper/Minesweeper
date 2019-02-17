6ee7789605055a8a521eaf364958f23d3014a6a6
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-17 10:31:31 +0100

    Added click function

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
+var cells = [];
+
 function setup() {
     createCanvas(600, 600);
-  }
-  
-function draw() {
-    background(255);
-    let gridSize = 8;
-    let cellSize = 32;
 
-    fill("#1111");
-    let cells = [];
+    let gridSize = 8;
+    let cellSize = 50;
 
     for (var i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            let cell = new Cell(0+(50*j),0+(50*i));
-            cell.show();
+            let cell = new Cell(0+(cellSize*j),0+(cellSize*i), cellSize);
             cells.push(cell);
+            console.log(cell);
         }
+    }
+}
+
+function draw() {
+    for(let i = 0; i<cells.length; i++){
+        cells[i].show();
+    }
+}
+
+mousePressed = () => {
+    for(let i = 0; i<cells.length; i++){
+        cells[i].clicked();
     }
 }
