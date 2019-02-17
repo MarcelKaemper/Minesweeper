4f141e907a0773a3ceaf7b1e66bfc13fd044aa22
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-17 11:10:52 +0100

    App.js now draws a minesweeper field

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ function setup() {
     let gridSize = 8;
     let cellSize = 50;
 
-    for (var i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            let cell = new Cell(0+(cellSize*j),0+(cellSize*i), cellSize);
+    let field = [["x","1","0","0","0","0"],
+                ["1","2","1","2","1","1"],
+                ["0","1","x","2","x","1"],
+                ["0","1","1","2","1","1"]];
+    
+    for(let i = 0; i<field.length; i++){
+        for(let j = 0; j<field[i].length; j++){
+            let color;
+            if(field[i][j] == "x"){
+                color = "#ff384b";
+            }else if(field[i][j] == "0"){
+                color = "#fff";
+            }else{
+                color = "#000";
+            }
+            let cell = new Cell(0+(cellSize*j),0+(cellSize*i), cellSize, color, field[i][j]);
             cells.push(cell);
-            console.log(cell);
         }
     }
+
+    // for (var i = 0; i < gridSize; i++) {
+    //     for (let j = 0; j < gridSize; j++) {
+    //         let cell = new Cell(0+(cellSize*j),0+(cellSize*i), cellSize);
+    //         cells.push(cell);
+    //         console.log(cell);
+    //     }
+    // }
 }
 
 function draw() {
--- cell.js
+++ cell.js
@@ -1,14 +1,17 @@
 class Cell {
-    constructor(x,y,size) {
+    constructor(x,y,size,color,text) {
        this.x = x; 
        this.y = y; 
        this.size = size;
-       this.col = "#123654";
+       this.col = color;
+       this.text = text;
     }
 
     show = () => {
         fill(this.col)
         rect(this.x,this.y,this.size,this.size);
+        fill(128);
+        text(this.text,this.x+25,this.y+25);
     }
 
     clicked = () => {
